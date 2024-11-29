c9ed4b86eeffcfa650037c13448ed00e51503495
rainmanxp <rainmanxp@gmail.com>
2024-11-29 18:20:17 +0800

    sql

--- database_script/rds/init_db.sql
+++ database_script/rds/init_db.sql
@@ -1,1 +1,1 @@
-CREATE TABLE `user_tbl` ( `name` varchar(64) NULL, `age` smallint(8) NULL ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+CREATE TABLE IF NOT EXISTS `user_tbl` ( `name` varchar(64) NULL, `age` smallint(8) NULL ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
