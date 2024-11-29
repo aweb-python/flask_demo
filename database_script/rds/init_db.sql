a7ae33ee876ab55792040dca05dcfe28132f6fc1
rainmanxp <rainmanxp@gmail.com>
2024-11-29 18:01:40 +0800

    modify db script

--- database_script/rds/init_db.sql
+++ database_script/rds/init_db.sql
@@ -1,1 +1,1 @@
-CREATE TABLE `user_tbl` ( `name` varchar(64) NULL, `age` smallint(8) NULL ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8
+CREATE TABLE `user_tbl` ( `name` varchar(64) NULL, `age` smallint(8) NULL ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
--- database_script/rds/insert_db.sql
+++ database_script/rds/insert_db.sql
@@ -1,3 +1,2 @@
-
 INSERT INTO `testdb`.user_tbl values('cat',22);
 INSERT INTO `testdb`.user_tbl values('tom',12);
